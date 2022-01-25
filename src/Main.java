1d841eb2fde0f5fbeb5f43f16de23e2a4bacd803
ZimovOleg <zimov.oleg@gmail.com>
2022-01-25 14:32:53 +0400

    add src/

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int currentAccount = 2_000_000_000;
+        int transfer = 500_000_000;
+        int total = currentAccount + transfer;
+        System.out.println(total);
+    }
+}
